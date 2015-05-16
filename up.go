3052463094ae76ab60fd552a489560dc95fdd6dc
Steven Richards <steven@axiomzen.co>
2015-05-16 20:32:28 +0200

    added list support

--- up.go
+++ up.go
@@ -4,28 +4,25 @@ import (
 	"flag"
 	"fmt"
 	"net"
+	"strings"
 	"time"
 )
 
 var (
 	host       string
 	protocol   string
-	port       string
+	ports      string
 	interval   int
 	timeout    int
 	runTimeout int
-	verbose    string
 )
 
 func init() {
 	flag.StringVar(&host, "host", "", "target host -host=x")
 	flag.StringVar(&host, "h", "", "target host -h=x (shorthand)")
-	flag.StringVar(&protocol, "proto", "tcp", "target protocol -proto=<tcp/udp>")
+	flag.StringVar(&protocol, "protos", "tcp", "target protocol -proto=<tcp/udp>")
 	flag.StringVar(&protocol, "pr", "tcp", "target protocol -pr=<tcp/udp> (shorthand)")
-	flag.StringVar(&port, "port", "", "target port -port=x")
-	flag.StringVar(&port, "p", "", "target port -p=x (shorthand)")
-	flag.StringVar(&verbose, "verbose", "false", "target port -verbose=x")
-	flag.StringVar(&verbose, "v", "false", "target port -v=x (shorthand)")
+	flag.StringVar(&ports, "ports", "", "list of ports to test -ports=x,x,x")
 	flag.IntVar(&interval, "interval", 1, "interval to test -interval=x")
 	flag.IntVar(&interval, "i", 1, "interval to test -i=x (shorthand)")
 	flag.IntVar(&timeout, "timeout", 1, "timeout -timeout=x")
@@ -33,26 +30,50 @@ func init() {
 	flag.Parse()
 }
 func main() {
-	if host == "" || port == "" {
+	if host == "" || ports == "" {
 		flag.PrintDefaults()
 		return
 	}
-	if verbose == "true" {
-		fmt.Println(host)
-		fmt.Println(protocol)
-		fmt.Println(port)
-		fmt.Println(interval)
-		fmt.Println(timeout)
-	}
-	for {
-		conn, _ := net.DialTimeout(protocol, host+":"+port, time.Second*(time.Duration(timeout)))
-		if verbose == "true" {
-			fmt.Println(conn)
+	protoList := strings.Split(protocol, ",")
+	portsList := strings.Split(ports, ",")
+	if len(protoList) == len(portsList) {
+		for Idx, portIdx := range portsList {
+			for to := 0; to <= timeout; to++ {
+				conn, _ := net.DialTimeout(protoList[Idx], host+":"+portIdx, time.Second*(time.Duration(timeout)))
+				if conn != nil {
+					fmt.Printf("OK! - %v - %v - %v \n", host, portIdx, protoList[Idx])
+					break
+				} else if to == timeout {
+					fmt.Printf("Fail! - %v - %v\n", host, portIdx)
+				}
+				time.Sleep(time.Second * (time.Duration(interval)))
+			}
 		}
-		if conn != nil {
-			fmt.Print(host)
-			return
+	} else if protocol == "tcp" {
+		for _, portIdx := range portsList {
+			for to := 0; to <= timeout; to++ {
+				conn, _ := net.DialTimeout("tcp", host+":"+portIdx, time.Second*(time.Duration(timeout)))
+				if conn != nil {
+					fmt.Printf("OK! - %v - %v - %v \n", host, portIdx, "tcp")
+					break
+				} else if to == timeout {
+					fmt.Printf("Fail! - %v - %v - %v \n", host, portIdx, "tcp")
+				}
+				time.Sleep(time.Second * (time.Duration(interval)))
+			}
 		}
-		time.Sleep(time.Second * (time.Duration(interval)))
+	} else if protocol == "udp" {
+		for _, portIdx := range portsList {
+			for to := 0; to <= timeout; to++ {
+				conn, _ := net.DialTimeout("udp", host+":"+portIdx, time.Second*(time.Duration(timeout)))
+				if conn != nil {
+					fmt.Printf("OK! - %v - %v - %v \n", host, portIdx, "udp")
+					break
+				} else if to == timeout {
+					fmt.Printf("Fail! - %v - %v - %v \n", host, portIdx, "udp")
+				}
+				time.Sleep(time.Second * (time.Duration(interval)))
+			}
+		}
 	}
 }
